7a694421ae338cac7ff699378add70e52aa0d812
Manuel Carrier-Nunes <manuel.carrier-nunes@outlook.com>
2021-02-01 22:09:24 -0500

    reduce text

--- non-terminating-error.ps1
+++ non-terminating-error.ps1
@@ -4,4 +4,4 @@ Folder abc doesn't exist. Thus, 'Get-ChildItem abc'
 - shouldn't terminate the script's execution; line 7 should execute.
 #>
 Get-ChildItem abc
-Write-Host 'This message should print even though there was an error on line 6.'
+Write-Host 'This message should print even though an error occurred on line 6.'
